727bd5da5cb3f7abcd168121420c4070f9f3af9f
Luis Farias <60280254+fariasByte@users.noreply.github.com>
2022-10-29 21:24:17 -0400

    Organizando código

--- empleados_lista.pas
+++ empleados_lista.pas
@@ -1,6 +1,7 @@
 program listaempleados;
+
 uses
-crt; {Unidad que proporciona acceso avanzado a la pantalla y teclado}
+  crt;
 
 const
 	total = 1000;
@@ -30,8 +31,8 @@ procedure entradadatos;
   begin
     write('¿Cuántos empleados va a agregar esta vez? ');
     readln(cantidad);
-     for i := 1 to cantidad do
-     begin
+    for i := 1 to cantidad do
+      begin
         clrscr;
         writeln('  Ingrese los datos de los empleados : ',i, ' de: ', cantidad);
         writeln;
@@ -42,110 +43,122 @@ procedure entradadatos;
         write('Día de nacimiento: ');
         readln(listado[i].diaNac);
         if (listado[i].diaNac>31) or (listado[i].diaNac<1)  then
-        begin
-        repeat
-        write('Incorrecto, ingrese un número del 1 al 31: ');
-        readln(listado[i].diaNac);
-        until (listado[i].diaNac<=31) and (listado[i].diaNac>=1) 
+          begin
+          repeat
+          write('Incorrecto, ingrese un número del 1 al 31: ');
+          readln(listado[i].diaNac);
+          until (listado[i].diaNac<=31) and (listado[i].diaNac>=1) 
         end;
-		write('Mes de nacimiento: ');
+		    write('Mes de nacimiento: ');
         readln(listado[i].mesNac);
         if (listado[i].mesNac>12) or (listado[i].mesNac<1)  then
-        begin
-        repeat
-        write('Incorrecto, ingrese un número del 1 al 12: ');
-        readln(listado[i].diaNac);
-        until (listado[i].diaNac<=12) and (listado[i].diaNac>=1)
-        end;
-		write('Año de nacimiento: ');
-        readln(listado[i].anNac); 
-		listado[i].edad := anActual-listado[i].anNac;
-		if (listado[i].mesNac>mesActual) or ((listado[i].mesNac=mesActual) and (listado[i].diaNac>diaActual)) then
-        begin
-            listado[i].edad := listado[i].edad-1;
-        end;
+          begin
+          repeat
+          write('Incorrecto, ingrese un número del 1 al 12: ');
+          readln(listado[i].diaNac);
+          until (listado[i].diaNac<=12) and (listado[i].diaNac>=1)
+          end;
+          write('Año de nacimiento: ');
+          readln(listado[i].anNac); 
+          listado[i].edad := anActual-listado[i].anNac;
+          if (listado[i].mesNac>mesActual) or ((listado[i].mesNac=mesActual) and (listado[i].diaNac>diaActual)) then
+          begin
+          listado[i].edad := listado[i].edad-1;
+          end;
         write('Sueldo: ');
         readln(listado[i].sueldo);
-    if (listado[i].sueldo < 1000) or (listado[i].sueldo > 3000) then
-      begin
-      repeat
-        write('Incorrecto, ingrese un sueldo entre 1000 y 3000: ');
-        readln(listado[i].sueldo);
-      until (listado[i].sueldo > 1000) and (listado[i].sueldo < 3000)
-      end;
+        if (listado[i].sueldo < 1000) or (listado[i].sueldo > 3000) then
+          begin
+          repeat
+          write('Incorrecto, ingrese un sueldo entre 1000 y 3000: ');
+          readln(listado[i].sueldo);
+          until (listado[i].sueldo > 1000) and (listado[i].sueldo < 3000)
+        end;
 
 		case listado[i].sueldo of
-		    		1000..2000 :
-		    			begin
-		    				listado[i].categoria:=1;
-		    			end;
-		    		2001..2500 :
-		    			begin
-		    				listado[i].categoria:=2;
-		    			end;
-		    		2501..3000 :
-		    			begin
-		    				listado[i].categoria:=3;
-		    			end;
-		    	end; {end-case}
-    end;
+		  1000..2000 :
+		    begin
+		    	listado[i].categoria:=1;
+		    end;
+		  2001..2500 :
+		    begin
+		    	listado[i].categoria:=2;
+		    end;
+		  2501..3000 :
+		  	begin
+		    	listado[i].categoria:=3;
+		    end;
+		end; {end-case}
+  end;
+end;
+ 
+procedure ordenadatos;
+  begin
+    for t := 1 to cantidad do
+      for r := i downto t + 1 do
+        if listado[t].cedula > listado[r].cedula then
+          begin
+            aux := listado[t];
+            listado[t] := listado[r];
+            listado[r] := aux;
+          end;
   end;
  
-   procedure ordenadatos;
-   begin
-       for t := 1 to cantidad do
-         for r := i downto t + 1 do
-         if listado[t].cedula > listado[r].cedula then
-         begin
-             aux := listado[t];
-             listado[t] := listado[r];
-             listado[r] := aux;
-         end;
-   end;
+procedure mostrardatos;
+   
+  var
+    d: integer;
+  
+  begin   
+    gotoxy(30,1); writeln('.... Lista de empleados .....');
+    writeln;
+    gotoxy(1,3); 
+    writeln('N° de Cedula'); 
+    gotoxy(20,3); 
+    writeln('Nombre y Apellido');
+	  gotoxy(45,3); 
+    writeln('Edad'); 
+    gotoxy(55,3); 
+    writeln('Categoria');
+      for d := 1 to cantidad do
+        writeln(listado[d].cedula, ' ':11, listado[d].nombreApellido, ' ':15, listado[d].edad, ' ':10, listado[d].categoria);
+        writeln;
+        writeln;
+        writeln('*** Pulse [Enter] ***');
+        readln;
+  end;
  
-   procedure mostrardatos;
-   var
-      d: integer;
-   begin
-       
-       gotoxy(30,1); writeln('.... Lista de empleados .....');
-       writeln;
-       gotoxy(1,3); writeln('N° de Cedula'); gotoxy(20,3); writeln('Nombre y Apellido');
-	        gotoxy(45,3); writeln('Edad'); gotoxy(55,3); writeln('Categoria');
-       for d := 1 to cantidad do
-       writeln(listado[d].cedula, ' ':11, listado[d].nombreApellido, ' ':15, listado[d].edad, ' ':10, listado[d].categoria);
-       writeln;
-       writeln;
-       writeln('*** Pulse [Enter] ***');
-       readln;
-   end;
- 
-   procedure menu;
-   var
-     tec : char;
-   begin
+procedure menu;
+   
+  var
+    tec : char;
+  
+  begin
+    repeat
+      clrscr;
+      writeln('**** Menu General ****');
+      writeln;
+      writeln('  1 = Entrada Datos');
+      writeln('  2 = Mostrar Resultados');
+      writeln('  3 = Salir');
+      writeln;
+      writeln('<<< Indique una opción >>>');
       repeat
-       clrscr;
-       writeln('**** Menu General ****');
-       writeln;
-       writeln('  1 = Entrada Datos');
-       writeln('  2 = Mostrar Resultados');
-       writeln('  3 = Salir');
-       writeln;
-       writeln('<<< Indique una opción >>>');
-       repeat
-       tec := readkey;
-       until tec in['1','2','3'];
+      tec := readkey;
+      until tec in['1','2','3'];
     case tec of
-  '1' : begin
+    '1' : begin
         clrscr;
         entradadatos;
         ordenadatos;
         end;
-  '2' : begin clrscr; mostrardatos; end;
+    '2' : begin 
+        clrscr;
+        mostrardatos; 
+        end;
     end;
-      until tec = '3';
-   end;
+    until tec = '3';
+  end;
  
  
 begin
